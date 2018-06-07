3b28e3cf57fe104b97b0615325bdbbe4045613cc
Umeda <bbrnobby@gmail.com>
2018-06-07 18:07:45 +0900

    add Sign

---
+++ Mikoshikagura/Source/Sign.cpp
@@ -0,0 +1,28 @@
+#include "Sign.h"
+#define POPDELAY 3
+
+Sign::Sign(std::string message, Hukidashi * hukidashi)
+{
+	this->message = message;
+	this->hukidashi = hukidashi;
+	delay = -1;
+}
+
+void Sign::Update(void)
+{
+	if (delay>0)
+	{
+		delay--;
+	}
+	else if (delay == 0)
+	{
+		delay = -1;
+		this->hukidashi->Unpop();
+	}
+}
+
+void Sign::OnCollision(Object * object)
+{
+	this->hukidashi->Pop(this->message);
+	delay = POPDELAY;
+}
