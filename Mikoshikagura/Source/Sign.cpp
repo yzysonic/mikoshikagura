bfebee99098b36f11a71c1559e6a7c9fcd755579
Umeda <bbrnobby@gmail.com>
2018-06-12 23:12:51 +0900

    Sign 表示方法の変更

--- Mikoshikagura/Source/Sign.cpp
+++ Mikoshikagura/Source/Sign.cpp
@@ -1,28 +1,23 @@
 #include "Sign.h"
-#define POPDELAY 3
 
 Sign::Sign(std::string message, Hukidashi * hukidashi)
 {
 	this->message = message;
 	this->hukidashi = hukidashi;
-	delay = -1;
 }
 
 void Sign::Update(void)
 {
-	if (delay>0)
-	{
-		delay--;
-	}
-	else if (delay == 0)
-	{
-		delay = -1;
-		this->hukidashi->Unpop();
-	}
 }
 
-void Sign::OnCollisionStay(Object * object)
+void Sign::OnCollisionEnter(Object * object)
 {
-	this->hukidashi->Pop(this->message);
-	delay = POPDELAY;
+	if(object->type == ObjectType::Player)
+		this->hukidashi->Pop(this->message);
+}
+
+void Sign::OnCollisionExit(Object * object)
+{
+	if (object->type == ObjectType::Player)
+		this->hukidashi->Unpop();
 }
