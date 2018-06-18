6fed9e2fe386983e52dabe73259f1dc59fa5c9a0
nagai.takeru <nagai.takeru@DESKTOP-9VHHTDU>
2018-06-18 16:57:34 +0900

    カメラスナップテスト

--- Mikoshikagura/Source/Scene_Stage1.cpp
+++ Mikoshikagura/Source/Scene_Stage1.cpp
@@ -40,7 +40,7 @@ void Scene_Stage1::Init(void)
 	mapdata = new MapManager();
 	mapdata->Load("Data/Map/prototype_map1.tmx");
 	mapdata->SetPlayerpointer(player);
-
+	mapdata->SetSmoothPoint(camera);
 	hukidashi = new Hukidashi;
 
 	mapdata->SetSignText(hukidashi);
