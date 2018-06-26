63e3b0517f0c20fbde47e103f99e4e7f8b48756a
Umeda <bbrnobby@gmail.com>
2018-06-26 17:33:19 +0900

    背景レイヤー分けテスト

--- Mikoshikagura/Source/Background.cpp
+++ Mikoshikagura/Source/Background.cpp
@@ -3,8 +3,12 @@
 Background::Background(void)
 {
 	transform.position.y -= 20.0f;
-	image = AddComponent<RectPolygon2D>("background_summer", Layer::BG_00);
-	image->SetSize(1500.0f, 1125.0f);
+	image_layer0 = AddComponent<RectPolygon2D>("background_summer_layer0", Layer::BG_00);
+	image_layer0->SetSize(1500.0f, 1125.0f);
+	image_layer1 = AddComponent<RectPolygon2D>("background_summer_layer1", Layer::BG_01);
+	image_layer1->SetSize(1500.0f, 1125.0f);
+	image_layer2 = AddComponent<RectPolygon2D>("background_summer_layer2", Layer::BG_02);
+	image_layer2->SetSize(1500.0f, 1125.0f);
 	camera = &RenderSpace::Get("default")->GetCamera(0)->transform;
 }
 
@@ -12,20 +16,42 @@ void Background::Update(void)
 {
 	auto offset = (camera->position*0.001f).toVector2();
 	offset.y *= -1.0f;
-	image->vertex[0].uv = offset;
-	image->vertex[1].uv = Vector2(1.0f, 0.0f) + offset;
-	image->vertex[2].uv = Vector2(0.0f, 1.0f) + offset;
-	image->vertex[3].uv = Vector2(1.0f, 1.0f) + offset;
+	image_layer0->vertex[0].uv = offset;
+	image_layer0->vertex[1].uv = Vector2(1.0f, 0.0f) + offset;
+	image_layer0->vertex[2].uv = Vector2(0.0f, 1.0f) + offset;
+	image_layer0->vertex[3].uv = Vector2(1.0f, 1.0f) + offset;
+
+	image_layer1->vertex[0].uv = offset;
+	image_layer1->vertex[1].uv = Vector2(1.0f, 0.0f) + offset;
+	image_layer1->vertex[2].uv = Vector2(0.0f, 1.0f) + offset;
+	image_layer1->vertex[3].uv = Vector2(1.0f, 1.0f) + offset;
+
+	image_layer2->vertex[0].uv = offset;
+	image_layer2->vertex[1].uv = Vector2(1.0f, 0.0f) + offset;
+	image_layer2->vertex[2].uv = Vector2(0.0f, 1.0f) + offset;
+	image_layer2->vertex[3].uv = Vector2(1.0f, 1.0f) + offset;
 }
 
 void Background::SetSummer(void)
 {
-	image->SetTexture("background_summer");
-	image->SetSize(1500.0f, 1125.0f);
+	image_layer0->SetTexture("background_summer_layer0");
+	image_layer0->SetSize(1500.0f, 1125.0f);
+
+	image_layer1->SetTexture("background_summer_layer1");
+	image_layer1->SetSize(1500.0f, 1125.0f);
+
+	image_layer2->SetTexture("background_summer_layer2");
+	image_layer2->SetSize(1500.0f, 1125.0f);
 }
 
 void Background::SetWinter(void)
 {
-	image->SetTexture("background_winter");
-	image->SetSize(1500.0f, 1125.0f);
+	image_layer0->SetTexture("background_winter_layer0");
+	image_layer0->SetSize(1500.0f, 1125.0f);
+
+	image_layer1->SetTexture("background_winter_layer1");
+	image_layer1->SetSize(1500.0f, 1125.0f);
+
+	image_layer2->SetTexture("background_winter_layer2");
+	image_layer2->SetSize(1500.0f, 1125.0f);
 }
--- Mikoshikagura/Source/Background.h
+++ Mikoshikagura/Source/Background.h
@@ -11,6 +11,8 @@ public:
 	void SetWinter(void) override;
 
 private:
-	RectPolygon2D * image;
+	RectPolygon2D * image_layer0;
+	RectPolygon2D * image_layer1;
+	RectPolygon2D * image_layer2;
 	Transform* camera;
 };
