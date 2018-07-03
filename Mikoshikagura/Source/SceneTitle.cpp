bd46a615659efca1a834e23dec4ee4a3e75f8aa0
Umeda <bbrnobby@gmail.com>
2018-07-03 12:22:09 +0900

    タイトルロゴの追加

--- Mikoshikagura/Source/SceneTitle.cpp
+++ Mikoshikagura/Source/SceneTitle.cpp
@@ -8,12 +8,12 @@ void SceneTitle::Init(void)
 	FadeScreen::FadeIn(Color::black, 1.0f);
 	Light::Init();
 
-	Texture::Load("mikoshikagura_rogo");
+	Texture::Load("mikoshikagura_rogo2");
 
 	RenderSpace::Get("default")->GetCamera(0)->setBackColor(Color::black);
 
 	title = new Object;
-	title->AddComponent<RectPolygon2D>("mikoshikagura_rogo")->SetSize(960, 222);
+	title->AddComponent<RectPolygon2D>("mikoshikagura_rogo2")->SetSize(960, 240);
 	title->transform.position = Vector3(0, 100, 0);
 	title->AddComponent<Text>()->LoadFont("‚¨‚Â‚Æ‚ßƒtƒHƒ“ƒg");
 	title->GetComponent<Text>()->area = { 475,400,1000,500 };
@@ -40,4 +40,5 @@ void SceneTitle::Update(void)
 
 void SceneTitle::Uninit(void)
 {
+	Texture::Release("mikoshikagura_rogo2");
 }
